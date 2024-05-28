80b0ef17641ba2100877b1bfd6a776ae057afd70
Mark Julien Hahn <mail@markjulienhahn.de>
2024-05-28 16:47:09 +0200

    Logo Animation

--- components/Introslider.js
+++ components/Introslider.js
@@ -1,17 +1,32 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import logo from "../public/img/space-between-people-logo.gif";
 
 const Introslider = ({ images }) => {
-  // useEffect(() => {
-  //   return () => {
-  //     window.scrollTo({ top: 0, behavior: "smooth" });
-  //   };
-  // }, []);
+  const [scrollY, setScrollY] = useState(0);
+  const [isFaded, setIsFaded] = useState(false);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    setScrollY(currentScrollY);
+    setIsFaded(currentScrollY > 300);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="header">
+      <div
+        className={`logoContainer ${isFaded ? "fade-out" : ""}`}
+        style={{ transform: `translateY(-${scrollY / 4}px)` }}
+      >
+        <Image src={logo} width={500} height={500} />
+      </div>
       {images.map((image, i) => (
         <div key={i} className="sliderImageWrapper">
           <Image
